a32de46209457f929e44cdb46a75e621e3be60de
Atanu Nayak <93304796+Nayaker@users.noreply.github.com>
2022-01-19 19:04:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ const mongoose = require('mongoose');
 app.use(cors());
 app.use(express.json());
 
-// let data = [{
-//               name: "Node.js E-Commerce App REST API with MongoDB | Shopping API with Stripe & JWT",
-//               desc: "Node MongoDB eCommerce Rest API using Stripe payment method. Node.js shopping API tutorial using express, MongoDB, and JWT for beginners.",
-//               imgUrl: "https://i.ytimg.com/vi/rMiRZ1iRC0A/maxresdefault.jpg",
-//               ytLink: "https://www.youtube.com/watch?v=rMiRZ1iRC0A",
-//             },
-//             {
-//               name: '2021 YEAR END MASHUP - SUSH & YOHAN (BEST 130+ SONGS OF 2021)',
-//               desc: `Goodbye, 2021.  "We've all gone through some of our darkest times this year, but we have also found our own ways to keep going. This year might not be the best year for some of us as it broke you but healed as well, resisted your growth but not you.  The new year wouldn't change anything, but it definitely brings a sense of optimism  - to escape our undeniable reality into our dream world, of our own!"`,
-//               imgUrl: 'https://i.ytimg.com/vi/uX7PFOsG4F4/maxresdefault.jpg',
-//               ytLink: 'https://www.youtube.com/watch?v=uX7PFOsG4F4'
-//             }
-// ];
-
 const schema = new mongoose.Schema({
   subject: String,
   description: String,
@@ -61,7 +47,7 @@ app.post('/assignments', (req, res) => {
   }).catch((err) => res.json({error : "Can't save to mongo!"}));
 })
 
-app.listen(port || process.env.port, ()=> {
+app.listen(port || process.env.PORT, ()=> {
   console.log(`Example app listening at http://localhost:${port}`)
   mongoose.connect(CONNECTION_URL,{
       useNewUrlParser: true ,
