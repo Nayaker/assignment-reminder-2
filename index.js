1e3c106573bf9b55ca44dbe377da483f467db5d6
Anurag Chakraborty <81907012+achak47@users.noreply.github.com>
2022-01-19 19:36:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const app = express()
-const port = 8000;
+const port = 3000;
 const mongoose = require('mongoose');
 
 
